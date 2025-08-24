093e42ecff938febe452bee880fcdeeca2b14ead
emirtotic <emirtotic@gmail.com>
2025-08-24 23:22:16 +0200

    print

--- src/main/java/com/arrays/MozeZeros.java
+++ src/main/java/com/arrays/MozeZeros.java
@@ -28,4 +28,15 @@ public class MozeZeros {
             }
         }
     }
+
+    public static void print(int[] array) {
+        System.out.print("[ ");
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.print(array[i] + " ]\n");
+            } else {
+                System.out.print(array[i] + " | ");
+            }
+        }
+    }
 }
