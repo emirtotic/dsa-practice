3afd1192e0937c447a0f3d56201a89370b2bffe5
emirtotic <emirtotic@gmail.com>
2025-08-26 13:39:51 +0200

    Palindrome 2

---
+++ src/main/java/com/arrays/PalindromeSpecialChars.java
@@ -0,0 +1,38 @@
+package com.arrays;
+
+public class PalindromeSpecialChars {
+
+    public static void main(String[] args) {
+
+        String word = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindrome(word));
+
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+
+}
