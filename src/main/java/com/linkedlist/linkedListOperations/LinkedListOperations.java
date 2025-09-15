07325708f7be9430886a97e2d007d4b56c140e00
emirtotic <emirtotic@gmail.com>
2025-09-15 13:29:42 +0200

    Linked list

---
+++ src/main/java/com/linkedlist/linkedListOperations/LinkedListOperations.java
@@ -0,0 +1,117 @@
+package com.linkedlist.linkedListOperations;
+
+public class LinkedListOperations {
+
+    ListNode head;
+
+
+    public void print() {
+
+        ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.val + "   ");
+            current = current.next;
+        }
+
+        System.out.println();
+
+    }
+
+    public int countElements() {
+
+        int counter = 0;
+
+        ListNode current = head;
+
+        while (current != null) {
+            counter++;
+            current = current.next;
+        }
+
+        return counter;
+
+    }
+
+    public int findMax() {
+
+        ListNode current = head;
+        ListNode max = head;
+
+        while (current != null) {
+
+            if (current.val > max.val) {
+                max = current;
+            }
+
+            current = current.next;
+
+        }
+
+        return max.val;
+
+    }
+
+    public void addFirst(ListNode node) {
+        node.next = head; // novi čvor pokazuje na stari head
+        head = node;      // pomeri head na novi čvor
+    }
+
+    public void addLast(ListNode node) {
+
+        if (head == null) {
+            head = node;
+            return;
+        }
+
+        ListNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = node;
+
+    }
+
+    public void deleteFirst() {
+        head = head.next;
+    }
+
+    public void deleteLast() {
+        if (head == null) {
+            return;
+        }
+
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        ListNode current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+
+        current.next = null;
+    }
+
+    public ListNode findValue(int target) {
+
+        ListNode current = head;
+
+        while (current != null) {
+
+            if (current.val == target) {
+                return current;
+            }
+
+            current = current.next;
+
+        }
+
+        return null;
+
+    }
+
+
+}
---
+++ src/main/java/com/linkedlist/linkedListOperations/Main.java
@@ -0,0 +1,44 @@
+package com.linkedlist.linkedListOperations;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedListOperations list = new LinkedListOperations();
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(10);
+        ListNode node3 = new ListNode(20);
+        ListNode node4 = new ListNode(15);
+        ListNode node5 = new ListNode(122);
+        ListNode node6 = new ListNode(70);
+
+        list.head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        list.print();
+        System.out.println("Total elements: " + list.countElements());
+        System.out.println("Max element: " + list.findMax());
+        System.out.println("Add first:");
+        list.addFirst(new ListNode(0));
+        list.print();
+        System.out.println("Add last:");
+        list.addLast(new ListNode(500));
+        list.print();
+        System.out.println("Delete first:");
+        list.deleteFirst();
+        list.print();
+        System.out.println("Delete last:");
+        list.deleteLast();
+        list.print();
+        System.out.println("Find node:");
+        System.out.println(list.findValue(15));
+
+
+
+    }
+}
