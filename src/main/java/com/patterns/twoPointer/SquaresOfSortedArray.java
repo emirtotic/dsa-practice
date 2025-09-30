d1ec1b9dde569b2b5dfc433818d74a624a3a6145
Emir Totic <emirtotic@gmail.com>
2025-09-30 14:49:21 +0200

    SquaresOfSortedArray 977

---
+++ src/main/java/com/patterns/twoPointer/SquaresOfSortedArray.java
@@ -0,0 +1,39 @@
+package com.patterns.twoPointer;
+
+public class SquaresOfSortedArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {-100, -2, -1, 0, 3, 4};
+        sortedSquares(array);
+
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        int head = 0;
+        int tail = nums.length - 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            if (nums[head] > nums[tail]) {
+                result[i] = nums[head];
+                head++;
+            } else {
+                result[i] = nums[tail];
+                tail--;
+            }
+
+        }
+
+        return result;
+    }
+
+
+}
