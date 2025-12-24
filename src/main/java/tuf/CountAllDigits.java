0b8fb7124c41582ce53cfb851a16dbc7f34cb78a
Emir Totic <emirtotic@gmail.com>
2025-12-24 17:02:50 +0100

    CountAllDigits

--- src/main/java/tuf/CountAllDigits.java
+++ src/main/java/tuf/CountAllDigits.java
@@ -1,4 +1,26 @@
 package tuf;
 
 public class CountAllDigits {
+
+    public static void main(String[] args) {
+
+        int num = 3242724;
+        System.out.println(countDigit(num));
+    }
+
+    public static int countDigit(int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int count = 0;
+
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+
 }
